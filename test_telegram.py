ae8e5a60249af297ffba561958f67f681fc3c441
ouissalthoe <ouissalboudifa@gmail.com>
2026-06-28 18:47:52 +0400

    Add Telegram bot message sending functionality

--- test_telegram.py
+++ test_telegram.py
@@ -1,1 +1,8 @@
+from telegram import Bot
 
+BOT_TOKEN = "8868846049:AAE6syp1iH8NXv2y0ehsSBiVJcdLUAmHy3g"
+CHAT_ID = 8419437999
+
+bot = Bot(token=BOT_TOKEN)
+bot.send_message(chat_id=CHAT_ID, text="🔥 TEST MESSAGE")
+print("sent")
